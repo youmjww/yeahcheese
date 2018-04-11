f6d5f17c55161b75c8716d449cf428e4f50fbca4
youmjww <yuya-ohmura@sencorp.co.jp>
2018-04-11 18:26:00 +0900

    1 - テーブル作成時のコードに改行を入れた

--- youmjww.sql
+++ youmjww.sql
@@ -1,14 +1,30 @@
 // usersテーブルの作成
-CREATE TABLE users ( id INTEGER, mailaddress TEXT, password TEXT);
+CREATE TABLE users (
+                     id INTEGER, 
+                     mailaddress TEXT, 
+                     password TEXT
+                   );
 // user id用のシーケンス作成 
 CREATE SEQUENCE user_id START WITH 1;
 
 // photosテーブルの作成
-CREATE TABLE photos (id INTEGER, name TEXT, base64 TEXT, event_id INTEGER);
+CREATE TABLE photos (
+                      id INTEGER,
+                      name TEXT, 
+                      base64 TEXT, 
+                      event_id INTEGER
+                    );
 // photo id用のシーケンスを作成
 CREATE SEQUENCE event_id START WITH 1;
 
 // eventテーブルの作成
-CREATE TABLE events (id INTEGER, open_day TIMESTAMP, end_day tTIMESTAMP event_name TEXT, user_id INTEGER, auth_key TEXT);
+CREATE TABLE events (
+                      id INTEGER,
+                      open_day TIMESTAMP,
+                      end_day tTIMESTAMP,
+                      event_name TEXT,
+                      user_id INTEGER,
+                      auth_key TEXT
+                    );
 // event id用のシーケンスを作成
 CREATE SEQUENCE photo_id START WITH 1;
