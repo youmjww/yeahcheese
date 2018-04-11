4e4a8e83ad8e01dd886a5957e4b0fe497efabc5f
youmjww <yuya-ohmura@sencorp.co.jp>
2018-04-11 18:46:35 +0900

    1 - クリエイトシーケンスを削除

--- youmjww.sql
+++ youmjww.sql
@@ -4,8 +4,6 @@ CREATE TABLE users (
                      mailaddress  VARCHAR(256) UNIQUE NOT NULL,
                      password     VARCHAR(64) UNIQUE NOT NULL
                    );
-// user id用のシーケンス作成 
-CREATE SEQUENCE user_id START WITH 1;
 
 // photosテーブルの作成
 CREATE TABLE photos (
@@ -14,8 +12,6 @@ CREATE TABLE photos (
                       base64    TEXT,
                       event_id  INTEGER
                     );
-// photo id用のシーケンスを作成
-CREATE SEQUENCE event_id START WITH 1;
 
 // eventテーブルの作成
 CREATE TABLE events (
@@ -26,5 +22,3 @@ CREATE TABLE events (
                       user_id     INTEGER UNIQUE NOT NULL,
                       auth_key    VARCHAR(9) UNIQUE NOT NULL
                     );
-// event id用のシーケンスを作成
-CREATE SEQUENCE photo_id START WITH 1;
