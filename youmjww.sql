02ec56b7f263f2eb49353dd9f01037f6b0b9f26e
youmjww <yuya-ohmura@sencorp.co.jp>
2018-04-11 18:54:53 +0900

    1 - COMMENT ON でコメントを書いて//コメントを削除

--- youmjww.sql
+++ youmjww.sql
@@ -1,11 +1,11 @@
-// usersテーブルの作成
+COMMENT ON TABLE users IS 'ユーザテーブルの作成';
 CREATE TABLE users (
                      id           INTEGER UNIQUE NOT NULL PRIMARY KEY,
                      mailaddress  VARCHAR(256) UNIQUE NOT NULL,
                      password     VARCHAR(64) UNIQUE NOT NULL
                    );
 
-// photosテーブルの作成
+COMMENT ON TABLE photos IS 'photosテーブルの作成';
 CREATE TABLE photos (
                       id        INTEGER UNIQUE NOT NULL PRIMARY KEY,
                       name      TEXT UNIQUE NOT NULL,
@@ -13,7 +13,7 @@ CREATE TABLE photos (
                       event_id  INTEGER
                     );
 
-// eventテーブルの作成
+COMMENT ON TABLE events IS 'eventテーブルの作成';
 CREATE TABLE events (
                       id          INTEGER PRIMARY KEY,
                       open_day    TIMESTAMP UNIQUE NOT NULL,
