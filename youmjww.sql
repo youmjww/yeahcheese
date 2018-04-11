bb7a03e2e2bc6da03bb1ea7dc35294dcdaef46d5
youmjww <yuya-ohmura@sencorp.co.jp>
2018-04-11 18:31:45 +0900

    1 - ユニークとnot nullを追加

--- youmjww.sql
+++ youmjww.sql
@@ -1,17 +1,17 @@
 // usersテーブルの作成
 CREATE TABLE users (
-                     id INTEGER, 
-                     mailaddress TEXT, 
-                     password TEXT
+                     id INTEGER UNIQUE NOT NULL, 
+                     mailaddress TEXT UNIQUE NOT NULL, 
+                     password TEXT UNIQUE NOT NULL
                    );
 // user id用のシーケンス作成 
 CREATE SEQUENCE user_id START WITH 1;
 
 // photosテーブルの作成
 CREATE TABLE photos (
-                      id INTEGER,
-                      name TEXT, 
-                      base64 TEXT, 
+                      id INTEGER UNIQUE NOT NULL,
+                      name TEXT UNIQUE NOT NULL, 
+                      base64 TEXT,
                       event_id INTEGER
                     );
 // photo id用のシーケンスを作成
@@ -20,11 +20,11 @@ CREATE SEQUENCE event_id START WITH 1;
 // eventテーブルの作成
 CREATE TABLE events (
                       id INTEGER,
-                      open_day TIMESTAMP,
-                      end_day tTIMESTAMP,
-                      event_name TEXT,
-                      user_id INTEGER,
-                      auth_key TEXT
+                      open_day TIMESTAMP UNIQUE NOT NULL,
+                      end_day tTIMESTAMP UNIQUE NOT NULL,
+                      event_name TEXT UNIQUE NOT NULL,
+                      user_id INTEGER UNIQUE NOT NULL,
+                      auth_key TEXT UNIQUE NOT NULL
                     );
 // event id用のシーケンスを作成
 CREATE SEQUENCE photo_id START WITH 1;
