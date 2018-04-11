6d89ed4b0ab211e35142bb8a7a7d8316e0f94aef
youmjww <yuya-ohmura@sencorp.co.jp>
2018-04-11 18:20:36 +0900

    1 - 予約語を大文字にした

--- youmjww.sql
+++ youmjww.sql
@@ -1,14 +1,14 @@
 // usersテーブルの作成
-create table users ( id integer, mailaddress text, password text, admin char(4));
+CREATE TABLE users ( id INTEGER, mailaddress TEXT, password TEXT, admin char(4));
 // user id用のシーケンス作成 
-create sequence user_id start with 1;
+CREATE SEQUENCE user_id START WITH 1;
 
 // photosテーブルの作成
-create table photos (id integer, name text, base64 text, event_id integer);
+CREATE TABLE photos (id INTEGER, name TEXT, base64 TEXT, event_id INTEGER);
 // photo id用のシーケンスを作成
-create sequence event_id start with 1;
+CREATE SEQUENCE event_id START WITH 1;
 
 // eventテーブルの作成
-create table events (id integer, open_day timestamp, end_day timestamp, event_name text, user_id integer, auth_key text);
+CREATE TABLE events (id INTEGER, open_day TIMESTAMP, end_day tTIMESTAMP event_name TEXT, user_id INTEGER, auth_key TEXT);
 // event id用のシーケンスを作成
-create sequence photo_id start with 1;
+CREATE SEQUENCE photo_id START WITH 1;
