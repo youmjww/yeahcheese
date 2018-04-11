9a16ec697f6c2cc02f4535c42c83590631cccf8c
youmjww <yuya-ohmura@sencorp.co.jp>
2018-04-11 18:40:15 +0900

    1 - 一部文字列の長さを制限した

--- youmjww.sql
+++ youmjww.sql
@@ -1,8 +1,8 @@
 // usersテーブルの作成
 CREATE TABLE users (
-                     id INTEGER UNIQUE NOT NULL, 
-                     mailaddress TEXT UNIQUE NOT NULL, 
-                     password TEXT UNIQUE NOT NULL
+                     id INTEGER UNIQUE NOT NULL,
+                     mailaddress VARCHAR(256) UNIQUE NOT NULL,
+                     password VARCHAR(64) UNIQUE NOT NULL
                    );
 // user id用のシーケンス作成 
 CREATE SEQUENCE user_id START WITH 1;
@@ -10,7 +10,7 @@ CREATE SEQUENCE user_id START WITH 1;
 // photosテーブルの作成
 CREATE TABLE photos (
                       id INTEGER UNIQUE NOT NULL,
-                      name TEXT UNIQUE NOT NULL, 
+                      name TEXT UNIQUE NOT NULL,
                       base64 TEXT,
                       event_id INTEGER
                     );
@@ -22,9 +22,9 @@ CREATE TABLE events (
                       id INTEGER,
                       open_day TIMESTAMP UNIQUE NOT NULL,
                       end_day tTIMESTAMP UNIQUE NOT NULL,
-                      event_name TEXT UNIQUE NOT NULL,
+                      event_name VARCHAR(30) UNIQUE NOT NULL,
                       user_id INTEGER UNIQUE NOT NULL,
-                      auth_key TEXT UNIQUE NOT NULL
+                      auth_key VARCHAR(9) UNIQUE NOT NULL
                     );
 // event id用のシーケンスを作成
 CREATE SEQUENCE photo_id START WITH 1;
