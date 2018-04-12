1691634f3ce2101be54d9ebb293de326b79b25dc
youmjww <yuya-ohmura@sencorp.co.jp>
2018-04-12 10:33:10 +0900

    4 - 最低限のテンプレートとアクションクラスを追加

---
+++ app/action/home.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ *  Index action implementation.
+ *
+ *  @author     {$author}
+ *  @access     public
+ *  @package    My
+ */
+class My_Action_home extends My_ActionClass
+{
+    /**
+     *  preprocess regist user action.
+     *
+     *  @access    public
+     *
+     *  @return    string  Forward name (null if no errors.)
+     */
+    public function prepare(): ?string
+    {
+        return null;
+    }
+
+    /**
+     *  Index action implementation.
+     *
+     *  @access    public
+     *
+     *  @return    string  Forward Name.
+     */
+    public function perform(): string
+    {
+        return 'home';
+    }
+}
