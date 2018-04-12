28fd4653191b81a8209e0a14b6ed42172293a614
youmjww <yuya-ohmura@sencorp.co.jp>
2018-04-12 17:58:30 +0900

    5 - フォームのバリデーションを追加

--- app/action/CreateEvent/Do.php
+++ app/action/CreateEvent/Do.php
@@ -1,5 +1,40 @@
 <?php
 
+class My_Form_CreateEventDo extends My_ActionForm
+{
+    const MAX_EVENT_NAME_LENGTH = 30;
+    /**
+     *  @access   protected
+     *
+     *  @var      array   form definition.
+     */
+    public $form = [
+        'eventName'    => [
+            'type'       => VAR_TYPE_STRING,
+            'name'       => 'イベント名',
+            'max'        => self::MAX_EVENT_NAME_LENGTH,
+            'required'   => true,
+        ],
+        'openDay'      => [
+            'type'       => VAR_TYPE_DATETIME,
+            'name'       => '公開開始日',
+            'regexp'    =>  '/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/',
+            'required'   => true,
+        ],
+        'endDay'      => [
+            'type'       => VAR_TYPE_DATETIME,
+            'name'       => '公開終了日',
+            'regexp'    =>  '/^[0-9]{4}-[0-9]{2}-[0-9]{2}$/',
+            'required'   => true,
+        ],
+        'photos'      => [
+            'type'       => VAR_TYPE_FILE,
+            'name'       => '写真',
+            'required'   => true,
+        ],
+    ];
+}
+
 /**
  *  Index action implementation.
  *
@@ -9,6 +44,20 @@
  */
 class My_Action_CreateEventDo extends My_LoginActionClass
 {
+     /**
+     *  preprocess regist user action.
+     *
+     *  @access    public
+     *  @return    string  Forward name (null if no errors.)
+     */
+    public function prepare(): ?string
+    {
+        if ($this->af->validate() > 0) {
+            return 'createEvent';
+        }
+
+        return null;
+    }
     /**
      *  Login action implementation.
      *
