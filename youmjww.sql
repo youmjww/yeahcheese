148fafb4583ac497eed476ba0b27cc0ff6bc30e2
youmjww <yuya-ohmura@sencorp.co.jp>
2018-04-12 09:24:26 +0900

    1 - idにSERIALを指定してDDLをすっきりさせた

--- youmjww.sql
+++ youmjww.sql
@@ -1,12 +1,12 @@
 CREATE TABLE users (
-                     id           INTEGER UNIQUE NOT NULL PRIMARY KEY,
+                     id           INTEGER UNIQUE SERIAL,
                      mailaddress  VARCHAR(256) UNIQUE NOT NULL,
                      password     VARCHAR(64) UNIQUE NOT NULL
                    );
 COMMENT ON TABLE users IS 'ユーザテーブルの作成';
 
 CREATE TABLE photos (
-                      id        INTEGER UNIQUE NOT NULL PRIMARY KEY,
+                      id        INTEGER UNIQUE SERIAL,
                       name      TEXT NOT NULL,
                       base64    TEXT,
                       event_id  INTEGER NOT NULL
@@ -14,7 +14,7 @@ CREATE TABLE photos (
 COMMENT ON TABLE photos IS 'photosテーブルの作成';
 
 CREATE TABLE events (
-                      id          INTEGER PRIMARY KEY,
+                      id          INTEGER UNIQUE SERIAL,
                       open_day    TIMESTAMP,
                       end_day     TIMESTAMP,
                       event_name  VARCHAR(30) UNIQUE NOT NULL,
