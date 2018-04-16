9c7a5bd1a79630d3677afa00b5191ebc298983ec
youmjww <yuya-ohmura@sencorp.co.jp>
2018-04-16 11:13:58 +0900

    5 - 生成された認証キーが重複している場合の処理を追加

--- app/My_ModelEvents.php
+++ app/My_ModelEvents.php
@@ -20,17 +20,23 @@ class My_ModelEvents
 
     public function createEvent($userId, $openDay, $endDay, $eventName)
     {
+        $sql = "
+            INSERT INTO events (open_day, end_day, event_name, user_id, auth_key)
+                 VALUES (?, ?, ?, ?, ?
+        )";
+
+        do {
+            $result = $this->db->getAssoc($sql, [$openDay . ' 00:00:00', $endDay . ' 00:00:00', $eventName, $userId, $this->createAuthKey()]);
+        } while ($result === false);
+    }
+
+    private function createAuthKey() {
         // 認証キー生成
         $str = array_merge(range('a', 'z'), range('0', '9'), range('A', 'Z'));
         $authKey = '';
         for ($i = 0; $i < 9; $i++) {
             $authKey .= $str[rand(0, count($str) - 1)];
         }
-
-        $sql = "
-            INSERT INTO events (open_day, end_day, event_name, user_id, auth_key)
-                 VALUES (?, ?, ?, ?, ?
-        )";
-        $this->db->getAssoc($sql, [$openDay . ' 00:00:00', $endDay . ' 00:00:00', $eventName, $userId, $authKey]);
+        return $authKey;
     }
 }
